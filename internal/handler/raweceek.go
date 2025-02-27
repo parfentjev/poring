9ba9f427a583c049182a12ffa21c643647789075
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-02-27 18:14:10 +0000

    change api endpoint for raweceeks handler

--- internal/handler/raweceek.go
+++ internal/handler/raweceek.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"codeberg.org/parfentjev/godrop/internal/godrop"
 )
+
+type countdownResponse struct {
+	Session    session     `json:"session"`
+	Countdowns []countdown `json:"countdowns"`
+}
 
 type session struct {
 	Summary   string `json:"summary"`
@@ -16,75 +20,58 @@ type session struct {
 	StartTime string `json:"startTime"`
 }
 
-const WEEK_SECONDS float64 = 604_800
-const SUPER_MAX_SECONDS float64 = 225
-const DOG_YEAR_SECONDS float64 = 4_505_142
-const BLINK_SECONDS float64 = 0.3
+type countdown struct {
+	Value float32 `json:"value"`
+	Unit  string  `json:"unit"`
+}
 
 func handleNextSesson(e *godrop.EventContext) {
 	if !e.Message.IsChannel() {
 		return
 	}
 
-	var (
-		unitSeconds      float64
-		responseTemplate string
-	)
+	var key string
 
 	switch e.Message.Text.Command() {
 	case "!ceeks":
-		unitSeconds = WEEK_SECONDS
-		responseTemplate = "%s begins in %.2f ceeks"
+		key = "ceeks"
 	case "!supermax":
-		unitSeconds = SUPER_MAX_SECONDS
-		responseTemplate = "%s begins in %.2f super max songs"
+		key = "super maxes"
 	case "!dogs":
-		unitSeconds = DOG_YEAR_SECONDS
-		responseTemplate = "%s begins in %.2f dog years"
+		key = "dog years"
 	case "!blinks":
-		unitSeconds = BLINK_SECONDS
-		responseTemplate = "%s begins in %e eye blinks"
+		key = "eye blinks"
 	default:
 		return
 	}
 
-	session, err := fetchNextSession()
+	data, err := fetchCountdown()
 	if err != nil {
-		log.Printf("failed to fetch next session; %v", err)
+		log.Printf("failed to fetch countdown; %v", err)
 		return
 	}
 
-	units, err := calculateUnits(session, unitSeconds)
-	if err != nil {
-		log.Printf("failed to calculate units; %v", err)
-		return
+	for _, countdown := range data.Countdowns {
+		if countdown.Unit == key {
+			response := fmt.Sprintf("%s begins in %.2f %s", data.Session.Summary, countdown.Value, countdown.Unit)
+			e.Sendf("PRIVMSG %s :%s 🎉", e.Message.Params[0], prism(response))
+			break
+		}
 	}
-
-	response := fmt.Sprintf(responseTemplate, session.Summary, units)
-	e.Sendf("PRIVMSG %s :%s 🎉", e.Message.Params[0], prism(response))
 }
 
-func fetchNextSession() (session, error) {
-	var session session
+func fetchCountdown() (countdownResponse, error) {
+	var data countdownResponse
 
-	resp, err := http.Get("https://raweceek.eu/api/sessions/next")
+	resp, err := http.Get("https://raweceek.eu/api/sessions/countdown")
 	if err != nil || resp.StatusCode != http.StatusOK {
-		return session, err
+		return data, err
 	}
 
 	defer resp.Body.Close()
-	if err := json.NewDecoder(resp.Body).Decode(&session); err != nil {
-		return session, err
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return data, err
 	}
 
-	return session, nil
-}
-
-func calculateUnits(session session, n float64) (float64, error) {
-	startTime, err := time.Parse("2006-01-02T15:04Z", session.StartTime)
-	if err != nil {
-		return 0, err
-	}
-
-	return (float64(startTime.Unix()) - float64(time.Now().Unix())) / n, nil
+	return data, nil
 }
