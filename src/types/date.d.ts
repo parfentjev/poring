767f342ebdab23f6b8b3fb2e1d8ff0b4ea69d20a
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-05-12 20:53:06 +0300

    describe IDateProvider

--- src/types/date.d.ts
+++ src/types/date.d.ts
@@ -1,3 +1,9 @@
 export interface IDateProvider {
+  /**
+   * Returns the current date and time.
+   *
+   * @method now
+   * @returns {Date} The current date and time.
+   */
   now: () => Date
 }
---
+++ tests/src/irc/sasl.spec.ts
@@ -0,0 +1,5 @@
+describe('Sasl', () => {
+  describe('SaslAuthenticator', () => {
+    // todo
+  })
+})
