b8ef916881c9c64daf8286fb04979e2ec4684e07
Aleksei Parfentjev <aleksei.parfentjev@tutamail.com>
2025-11-29 16:43:45 +0200

    Split handlers (#1)
    * add github workflow to run typescript linter
    
    * split handlers into separate files; handle exceptions
    
    * enable checks for unused vars and imports

--- src/irc/authenticator.ts
+++ src/irc/authenticator.ts
@@ -13,13 +13,13 @@ export class SASLAuthenticator {
     this.client.send('CAP REQ :sasl')
   }
 
-  private handleCap(context: EventContext) {
+  private async handleCap(context: EventContext) {
     if (context.message.params.length === 2 && context.message.params[1] === 'ACK' && context.message.text === 'sasl') {
       context.send('AUTHENTICATE PLAIN')
     }
   }
 
-  private handleAuth(context: EventContext) {
+  private async handleAuth(context: EventContext) {
     if (context.message.params.length > 0 && context.message.params[0] === '+') {
       const data = Buffer.concat([
         Buffer.from('\x00'),
@@ -32,7 +32,7 @@ export class SASLAuthenticator {
     }
   }
 
-  private handleAuthSuccess(context: EventContext) {
+  private async handleAuthSuccess(context: EventContext) {
     context.send('CAP END')
   }
 }
