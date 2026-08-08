e03307deb15c3ce9a78b81a208d17ca417d5fc83
Aleksei Parfentjev <aleksei@fakeplastictrees.ee>
2026-08-08 23:19:40 +0300

    fix issue with incorrect event processing

--- src/shutdown.ts
+++ src/shutdown.ts
@@ -3,10 +3,12 @@ import type { Logger } from 'pino'
 export function createAbortSignal(logger: Logger): AbortSignal {
   const abortController = new AbortController()
 
-  const events = ['SIGINT', 'SIGTERM']
+  const events: NodeJS.Signals[] = ['SIGINT', 'SIGTERM']
   for (const event of events) {
-    logger.info(`${event} received`)
-    abortController.abort()
+    process.once(event, () => {
+      logger.info(`${event} received`)
+      abortController.abort()
+    })
   }
 
   return abortController.signal
