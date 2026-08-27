92134e583a7ef093af42f0d9c90cbec7cecac2bf
Aleksei Parfentjev <aleksei@fakeplastictrees.ee>
2026-08-27 21:45:23 +0300

    migrate to raweceek api v2

--- src/listeners/raweceek.ts
+++ src/listeners/raweceek.ts
@@ -4,13 +4,16 @@ import type { ListenerRaweceekConfig } from '../config.js'
 import type { EventContext } from '../event.js'
 
 type Response = {
-  nextSession: {
-    summary: string
-    countdowns: {
-      type: string
-      value: string
-    }[]
-  }
+  upcoming_sessions: Session[]
+  race_week: boolean
+}
+
+type Session = {
+  summary: string
+  countdowns: {
+    type: string
+    value: string
+  }[]
 }
 
 export function registerRaweceekListeners(eventManager: ClientEvenetManager) {
@@ -29,26 +32,31 @@ async function onMessage(ctx: EventContext<State, PrivateMessage>) {
 }
 
 async function handle(config: ListenerRaweceekConfig, send: (message: string) => void, channel: string) {
-  const response = await fetch(`${config.serviceUrl}/api/status`, { signal: AbortSignal.timeout(1_000) })
+  const response = await fetch(`${config.serviceUrl}/api/v2/status`, { signal: AbortSignal.timeout(1_000) })
   if (response.ok === false) {
     throw new Error(`response status: ${response.status}`)
   }
 
-  const body = parseResponse(await response.json())
-  const countdown = body.nextSession.countdowns.find((c) => c.type === 'CEEKS')
+  const nextSession = parseResponse(await response.json())
+  const countdown = nextSession.countdowns.find((c) => c.type === 'CEEKS')?.value
   if (countdown === undefined) {
     throw new Error(`CEEKS countdown is missing: ${response}`)
   }
 
-  send(`PRIVMSG ${channel} :\x02${body.nextSession.summary}\x02 begins in ${countdown.value} 🎉`)
+  send(`PRIVMSG ${channel} :\x02${nextSession.summary}\x02 begins in ${countdown} 🎉`)
 }
 
-function parseResponse(response: unknown): Response {
-  const { nextSession } = response as Partial<Response>
-  if (nextSession === undefined) {
-    throw new Error(`nestSession is undefined: ${response}`)
+function parseResponse(response: unknown): Session {
+  const { upcoming_sessions: upcoming } = response as Partial<Response>
+  if (upcoming === undefined) {
+    throw new Error(`upcoming_sessions is undefined: ${response}`)
   }
 
+  if (!Array.isArray(upcoming) || upcoming.length === 0) {
+    throw new Error(`corrupted upcoming_sessions: ${response}`)
+  }
+
+  const nextSession = upcoming[0]!
   if (nextSession.summary === undefined) {
     throw new Error(`summary is undefined: ${response}`)
   }
@@ -57,5 +65,5 @@ function parseResponse(response: unknown): Response {
     throw new Error(`countdowns is undefined: ${response}`)
   }
 
-  return { nextSession }
+  return nextSession
 }
